6ce348b947bb9eb79f0afad4d2951a22f9d8597f
janvi <jn410076@dal.ca>
2021-12-04 12:47:50 -0400

    Testimonials UI to share recent 3 customer reviews

--- team8_lunchbox/src/components/Testimonials.jsx
+++ team8_lunchbox/src/components/Testimonials.jsx
@@ -1,9 +1,111 @@
 import React from "react";
-
+import styled from "styled-components";
+import avatar1 from "../assets/avatar1.jpg";
+import { imageZoomEffect, TitleStyles } from "./ReusableStyles";
 export default function Testimonials() {
   return (
-    <div id="testimonials">
-      <h1>Testimonials</h1>
-    </div>
+    <Section id="testimonials">
+      <div className="container">
+        <div className="title">
+          <h1>
+            <span>What</span> Customers Says
+          </h1>
+        </div>
+        <div className="testimonials">
+          <div className="testimonial">
+            <div className="image">
+              <img src={avatar1} alt="" />
+            </div>
+            <p>
+              Best quality food for good diet and health.
+            </p>
+          </div>
+          <div className="testimonial">
+            <div className="image">
+              <img src={avatar1} alt="" />
+            </div>
+            <p>
+              Food full of Nutritions and Taste.
+            </p>
+          </div>
+          <div className="testimonial">
+            <div className="image">
+              <img src={avatar1} alt="" />
+            </div>
+            <p>
+              I like the salad here which is really 
+              good in terms of taste and diet.
+            </p>
+          </div>
+        </div>
+      </div>
+    </Section>
   );
 }
+
+const Section = styled.section`
+  margin: 5vw;
+  background: linear-gradient(to right, #fc4958, #e85d04, #fc4958);
+  padding: 0.2rem;
+  border-radius: 1.5rem;
+  position: relative;
+  .container {
+    margin: 0.5rem;
+    padding-top: 1vw;
+    padding-bottom: 4vw;
+    background-color: white;
+    border-radius: 1rem;
+    ${TitleStyles};
+    .title {
+      position: absolute;
+      top: -1rem;
+      left: 25%;
+      padding: 0 2rem;
+      background-color: white;
+    }
+    .testimonials {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      gap: 6vw;
+      margin-top: 3vw;
+      .testimonial {
+        padding: 0 4vw;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        text-align: center;
+        gap: 1rem;
+        p {
+          font-size: 1.1rem;
+          line-height: 2rem;
+          letter-spacing: 0.1rem;
+          span {
+            color: #fc4958;
+          }
+        }
+        ${imageZoomEffect};
+        .image {
+          overflow: hidden;
+          width: max-content;
+          max-height: 10rem;
+          border-radius: 10rem;
+          img {
+            height: 10rem;
+          }
+        }
+      }
+    }
+  }
+  @media screen and (min-width: 260px) and (max-width: 1080px) {
+    .container {
+      .title {
+        position: initial;
+        background-color: transparent;
+      }
+      .testimonials {
+        flex-direction: column;
+      }
+    }
+  }
+`;
